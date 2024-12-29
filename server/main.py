f20c21e0ed429d8f9bcddfb371e5a957fbe233bb
ShashwatK07 <96937389+ShashwatK07@users.noreply.github.com>
2024-12-29 13:26:58 +0530

    minor changes

--- server/main.py
+++ server/main.py
@@ -108,6 +108,6 @@ def generate_sql():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
-    # port = int(os.environ.get("PORT", 8080))
-    # app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True,port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
